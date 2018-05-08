f41e9e820c8eeb308fada548cfa4a7fe082ab215
Phil Spelman <phil_spelman@hotmail.com>
2018-05-08 12:07:13 -0700

    First Commit: Base project

---
+++ src/main/java/com/philspelman/springparamsexercisehellohuman/SpringParamsExerciseHelloHumanApplication.java
@@ -0,0 +1,12 @@
+package com.philspelman.springparamsexercisehellohuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringParamsExerciseHelloHumanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringParamsExerciseHelloHumanApplication.class, args);
+    }
+}
---
+++ src/test/java/com/philspelman/springparamsexercisehellohuman/SpringParamsExerciseHelloHumanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.philspelman.springparamsexercisehellohuman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringParamsExerciseHelloHumanApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
